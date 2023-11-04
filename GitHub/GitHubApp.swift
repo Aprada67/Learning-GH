70dfe07957f3c890fc4af78bd689a38b9f57fdd1
Alberto Prada <albertojprada10@gmail.com>
2023-11-04 11:53:23 +0000

    Initial Commit

---
+++ GitHub/GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubApp.swift
+//  GitHub
+//
+//  Created by Alberto Jesús Prada Parmera on 04/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
